4a08974ba4ceb48d541daf0c450fb48f90576240
Iván Jensen <ivanerikj@gmail.com>
2020-10-16 23:06:06 -0300

    Agregada interface y directiva de shirt-list para stock

---
+++ src/app/shirt-list/Shirt.ts
@@ -0,0 +1,8 @@
+export interface Shirt{
+    team: string;
+    season: string;
+    type: string;
+    price: number;
+    stock: number;
+    image: string;
+}
--- src/app/shirt-list/shirt-list.component.ts
+++ src/app/shirt-list/shirt-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Shirt } from './Shirt';
 
 @Component({
   selector: 'app-shirt-list',
@@ -7,14 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShirtListComponent implements OnInit {
 
-  shirt = {
-    team: 'River Plate',
-    season: '19/20',
-    type: 'Titular',
-    price: 49.99,
-    stock: 5,
-    image: 'assets/img/river-plate.png'
-  }
+  shirts: Shirt[] = [
+    {
+      team: 'River Plate',
+      season: '19/20',
+      type: 'Titular',
+      price: 49.99,
+      stock: 5,
+      image: 'assets/img/river-plate.png',
+    },
+    {
+      team: 'Boca Juniors',
+      season: '19/20',
+      type: 'Titular',
+      price: 49.99,
+      stock: 5,
+      image: 'assets/img/boca-juniors.png',
+    },
+    {
+      team: 'River Plate',
+      season: '19/20',
+      type: 'Suplente',
+      price: 49.99,
+      stock: 5,
+      image: 'assets/img/river-plate-suplente.png',
+    },
+    {
+      team: 'Boca Juniors',
+      season: '19/20',
+      type: 'Suplente',
+      price: 49.99,
+      stock: 5,
+      image: 'assets/img/boca-juniors-suplente.png',
+    },
+    {
+      team: 'Racing Club',
+      season: '19/20',
+      type: 'Titular',
+      price: 49.99,
+      stock: 0,
+      image: 'assets/img/racing-club.png',
+    },
+  ];
+  
 
   constructor() { }
 
