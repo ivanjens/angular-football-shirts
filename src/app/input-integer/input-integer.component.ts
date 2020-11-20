0681953e9224e298beb119ea933c4151e321332e
Ivan Jensen <ivanerikj@gmail.com>
2020-11-20 15:19:32 -0300

    Selector de cantidad pasado a componente

--- src/app/input-integer/input-integer.component.ts
+++ src/app/input-integer/input-integer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Shirt } from '../shirt-list/Shirt';
 
 @Component({
   selector: 'app-input-integer',
@@ -9,7 +10,75 @@ export class InputIntegerComponent implements OnInit {
 
   constructor() { }
 
+  @Input()
+  quantity: number;
+
+  @Input()
+  max: number;
+
+  @Input()
+  min: number;
+
+  @Output()
+  quantityChange: EventEmitter<number> = new EventEmitter<number>();
+
   ngOnInit(): void {
+  }
+
+  upQuantity() : void{
+    if(this.quantity < this.max){
+      this.quantity++;
+      this.quantityChange.emit(this.quantity);
+    }
+  }
+
+  downQuantity() : void{
+    if(this.quantity > this.min){
+      this.quantity--;
+      this.quantityChange.emit(this.quantity);
+    }
+  }
+
+  setQuantity(event) : void{
+    let validKey: boolean = this.validateKey(event.key);
+    console.log(event.key);
+
+    if(validKey){
+      if(this.quantity > this.max){
+        this.quantity = this.max;
+      } else if(this.quantity < this.min || this.quantity == NaN){
+          this.quantity = 0;
+      }
+      this.quantityChange.emit(this.quantity);
+    } else {
+      this.quantity = 0;
+      this.quantityChange.emit(this.quantity);
+    }
+  }
+
+
+  // comprueba que la key presionada en el input sea valida
+  validateKey(key) : boolean{
+    let listOfNumbers: Array<number> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let listOfKeys: Array<string> = ['Backspace', 'Control', 'Alt', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+    let maxItemsQuantity: number = 0;
+
+    /*
+      Comprueba cual arreglo tiene más elementos y lo utiliza para iterar la key entrante y así no dejar cosas sin comprobar
+    */
+
+    if(listOfNumbers.length >= listOfKeys.length){
+      maxItemsQuantity = listOfNumbers.length;
+    } else{
+      maxItemsQuantity = listOfKeys.length;
+    }
+    
+    /* Comprueba si la tecla presionada está dentro de las teclas permitidas para seleccionar el stock*/
+    for(let i = 0; i < maxItemsQuantity; i++){
+      if((key == listOfNumbers[i] || key == listOfKeys[i]) && key != ' '){
+        return true;
+      }
+    }
   }
 
 }
--- src/app/shirt-list/shirt-list.component.ts
+++ src/app/shirt-list/shirt-list.component.ts
@@ -62,56 +62,5 @@ export class ShirtListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  upQuantity(shirt: Shirt) : void{
-    if(shirt.quantity < shirt.stock){
-      shirt.quantity++;
-    }
-  }
-
-  downQuantity(shirt: Shirt) : void{
-    if(shirt.quantity > 0){
-      shirt.quantity--;
-    }
-  }
-
-  setQuantity(event, shirt: Shirt) : void{
-    let validKey: boolean = this.validateKey(event.key);
-    console.log(event.key);
-
-    if(validKey){
-      if(shirt.quantity > shirt.stock){
-        shirt.quantity = shirt.stock;
-      } else if(shirt.quantity < 0 || shirt.quantity == NaN){
-          shirt.quantity = 0;
-      }
-    } else {
-      shirt.quantity = 0;
-    }
-  }
-
-  validateKey(key) : boolean{
-    let listOfNumbers: Array<number> = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let listOfKeys: Array<string> = ['Backspace', 'Control', 'Alt', 'ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
-    let maxItemsQuantity: number = 0;
-
-    /*
-      Comprueba cual arreglo tiene más elementos y lo utiliza para iterar la key entrante
-      Fue pensado por si en un futuro se añaden más teclas 'permitidas'
-    */
-
-    if(listOfNumbers.length >= listOfKeys.length){
-      maxItemsQuantity = listOfNumbers.length;
-    } else{
-      maxItemsQuantity = listOfKeys.length;
-    }
-    
-    /* Comprueba si la tecla presionada está dentro de las teclas permitidas para seleccionar el stock*/
-    for(let i = 0; i < maxItemsQuantity; i++){
-      if((key == listOfNumbers[i] || key == listOfKeys[i]) && key != ' '){
-        return true;
-      }
-    }
-  }
-
 }
 
